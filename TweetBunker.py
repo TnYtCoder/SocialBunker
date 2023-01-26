7cec1c6c50444ba024b9f3ac6292809ad79b9d5b
TnYtCoder <115485810+TnYtCoder@users.noreply.github.com>
2023-01-26 14:02:55 +0530

    Create TweetBunker.py

---
+++ TweetBunker.py
@@ -0,0 +1,33 @@
+import sys, time, random
+print("")
+print("")
+print("ㄒ几ㄚㄒ匚ㄖᗪ乇尺")
+print("")
+print("TWEETBURST✓")
+print("")
+InstaUid = input("Enter Username --- @ ")
+def progressBar(count_value, total, suffix=''):
+
+    bar_length = 100
+
+    filled_up_Length = int(round(bar_length* count_value / float(total)))
+
+    percentage = round(100.0 * count_value/float(total),1)
+
+    bar = '=' * filled_up_Length + '-' * (bar_length - filled_up_Length)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' %(bar, percentage, '%', suffix))
+
+    sys.stdout.flush()
+
+for i in range(11):
+
+    time.sleep(random.random())
+
+    progressBar(i,10)
+    
+print("@" + InstaUid + "    Patched Successfully !!")
+print("")
+print("")
+print("@TnYtCoder Github !!")
+print("")
